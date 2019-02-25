8b2f17712a9f4dc831a07586a8352d6bcecff79e
Yehuda Smilowitz <Ysmilowitz@fmm.com>
2019-02-25 17:12:23 -0500

    First commit

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,40 @@
+import { GET_CONTACTS, GET_CONTACT, UPDATE_CONTACT, DELETE_CONTACT, ADD_CONTACT } from '../actions/types';
+import { stat } from 'fs';
+import Contact from '../components/contacts/Contact';
+
+const initialState = {
+    contacts: [],
+    contact: {}
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case GET_CONTACTS:
+            return {
+                ...state,
+                contacts: action.payload
+            }
+        case GET_CONTACT:
+            return {
+                ...state,
+                contact: action.payload
+            }
+        case DELETE_CONTACT:
+            return {
+                ...state,
+                contacts: state.contacts.filter(contact => contact.id !== action.payload)
+            }
+        case ADD_CONTACT:
+            return {
+                ...state,
+                contacts: [action.payload, ...state.contacts]
+            }
+        case UPDATE_CONTACT:
+            return {
+                ...state,
+                contacts: state.contacts.map(contact => contact.id === action.payload.id ? (contact = action.payload) : contact)
+            }
+        default:
+            return state;
+    }
+}
